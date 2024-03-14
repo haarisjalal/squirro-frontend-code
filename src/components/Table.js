d068af82019ca424974355c90258254785f7d4f4
haarisjalal <haarisjalal@hotmail.com>
2024-03-14 14:03:17 -0300

    Adding commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -50,70 +50,70 @@ const Table = ({ stores, books, authors }) => {
           <div className="store" key={store.id}>
             <div className="container">
               <div className="left">
-            <div className="store-image">
-              {storeImage ? (
-                <img src={storeImage} alt="Store Image" />
-              ) : (
-                'Store image not available'
-              )}
+                <div className="store-image">
+                  {storeImage ? (
+                    <img src={storeImage} alt="Store Image" />
+                  ) : (
+                    'Store image not available'
+                  )}
+                </div>
+              </div>
+              <div className="right">
+                <div>
+                  <StarRating store={store} />
+                </div>
+                <h2>{store.attributes.name}</h2>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>Books</th>
+                      <th>Author</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                    {store.relationships.books?.data.length === 0 || !store.relationships.books?.data ? (
+                      <tr>
+                        <td colSpan="2">No books found</td>
+                      </tr>
+                    ) : (
+                      store.relationships.books?.data.map((book) => {
+                        const foundBook = books.find((b) => b.id === book.id);
+                        if (foundBook) {
+                          const authorId = foundBook.relationships.author?.data?.id;
+                          const author = authors.find((a) => a.id === authorId);
+                          const authorName = author?.attributes?.fullName || 'No author found';
+                          return (
+                            <tr key={foundBook.id}>
+                              <td>{foundBook.attributes.name}</td>
+                              <td>{authorName}</td>
+                            </tr>
+                          );
+                        } else {
+                          return (
+                            <tr key={book.id}>
+                              <td>{'No books found'}</td>
+                              <td>{'No author found'}</td>
+                            </tr>
+                          );
+                        }
+                      })
+                    )}
+                  </tbody>
+                </table>
+                <div className="store-info">
+                  <p><a href={store.attributes.website || '#'}>{store.attributes.website || 'Not available'}</a></p>
+                  <p>{formatDate(store.attributes.establishmentDate)}</p>
+                </div>
+                <div className="flag">
+                  {countryFlags[countryCode] ? (
+                    <img src={countryFlags[countryCode]} alt="Country Flag" />
+                  ) : (
+                    'Country flag not available'
+                  )}
+                </div>
+              </div>
             </div>
-            </div>
-            <div className="right">
-            <div>
-              <StarRating store={store} />
-            </div>
-            <h2>{store.attributes.name}</h2>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Books</th>
-                  <th>Author</th>
-                </tr>
-              </thead>
-              <tbody>
-                
-              {store.relationships.books?.data.length === 0 || !store.relationships.books?.data ? (
-  <tr>
-    <td colSpan="2">No books found</td>
-  </tr>
-) : (
-  store.relationships.books?.data.map((book) => {
-    const foundBook = books.find((b) => b.id === book.id);
-    if (foundBook) {
-      const authorId = foundBook.relationships.author?.data?.id;
-      const author = authors.find((a) => a.id === authorId);
-      const authorName = author?.attributes?.fullName || 'No author found';
-      return (
-        <tr key={foundBook.id}>
-          <td>{foundBook.attributes.name}</td>
-          <td>{authorName}</td>
-        </tr>
-      );
-    } else {
-      return (
-        <tr key={book.id}>
-          <td>{'No books found'}</td>
-          <td>{'No author found'}</td>
-        </tr>
-      );
-    }
-  })
-)}
-              </tbody>
-            </table>
-            <div className="store-info">
-              <p><a href={store.attributes.website || '#'}>{store.attributes.website || 'Not available'}</a></p>
-              <p>{formatDate(store.attributes.establishmentDate)}</p>
-            </div>
-            <div className="flag">
-              {countryFlags[countryCode] ? (
-                <img src={countryFlags[countryCode]} alt="Country Flag" />
-              ) : (
-                'Country flag not available'
-              )}
-            </div>
-          </div>
-          </div>
           </div>
         );
       })}
